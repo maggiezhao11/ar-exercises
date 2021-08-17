096ae4a6920dc08e5558aba6828d6676d53c7027
maggiezhao11 <maggiezhao06@hotmail.com>
2021-08-17 03:15:03 +0000

    add validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
-  validates :name, length: {minimum: 3}   #how to show character??
+  validates :name, length: {minimum: 3}   
   validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
   validate :must_carry_on_men_or_women_apparel
 

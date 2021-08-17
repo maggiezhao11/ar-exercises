e6bcdf317534fe7236c465d1214bb8d95e03bdb2
maggiezhao11 <maggiezhao06@hotmail.com>
2021-08-17 03:13:17 +0000

    add relationship to store

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, length: {minimum: 3}   #how to show character??
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :must_carry_on_men_or_women_apparel
+
+  def must_carry_on_men_or_women_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:must_carry_on_men_or_women_apparel, "Stores must carry at least one of the men's or women's apparel!")
+    end
+  end
 
 end
